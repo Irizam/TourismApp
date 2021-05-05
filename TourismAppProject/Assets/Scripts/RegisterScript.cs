bc8df1b478977ae88168f959772672c6e14bea40
Rodrigo Iriarte <65354123+Irizam@users.noreply.github.com>
2021-05-05 11:10:05 -0400

    Revert "Validation for register form"

--- TourismAppProject/Assets/Scripts/RegisterScript.cs
+++ TourismAppProject/Assets/Scripts/RegisterScript.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Text.RegularExpressions;
 
 public class RegisterScript : MonoBehaviour
 {
@@ -11,7 +9,7 @@ public class RegisterScript : MonoBehaviour
         secondSurnameInput, dayInput, monthInput, yearInput;
     public Dropdown countryDropdown, regionDropdown;
     public Button loginButton, registerButton;
-    public Text registerMessage, emailError, passwordError, repeatpasswordError, nameError, firstSurnameError, secondSurnameError, dateOfBirthError;
+    public Text registerMessage;
     List<string> regionsStringList = new List<string>();
     // Start is called before the first frame update
     void Start()
@@ -50,106 +48,6 @@ public class RegisterScript : MonoBehaviour
 
     void RegisterButtonOnClick()
     {
-        emailError.text="";
-        passwordError.text="";
-        repeatpasswordError.text="";
-        nameError.text="";
-        firstSurnameError.text="";
-        secondSurnameError.text="";
-        dateOfBirthError.text="";
-        registerMessage.text="";
-        try//validations
-        {
-            if (emailInput.text != "")//email ****VERIFICAR QUE NO SE REPITA EN LA BASE DE DATOS****
-            {
-                try
-                {
-                    System.Net.Mail.MailAddress m = new System.Net.Mail.MailAddress(emailInput.text);
-                }
-                catch (System.Exception)
-                {
-                    emailError.text = "Formato incorrecto.";
-                }
-            }else{
-                emailError.text = "El correo requerido.";
-            }
-
-            if (passwordInput.text != "")//password
-            {
-                if (passwordInput.text.Length < 8)
-                {
-                    passwordError.text="Mínimo 8 caracteres";
-                }
-            }else{
-                passwordError.text="Contraseña requerida.";
-            }
-
-            if (repeatPasswordInput.text != "")//repeat password
-            {
-                if (repeatPasswordInput.text != passwordInput.text)
-                {
-                    repeatpasswordError.text="Repita la contraseña.";
-                }
-            }else{
-                repeatpasswordError.text="Repita la contraseña.";
-            }
-
-            
-            Regex rgx = new Regex(@"^[a-zA-Z]{1,60}$");
-            if (nameInput.text != "")//name
-            {
-                if (!rgx.IsMatch(nameInput.text))
-                {
-                    nameError.text = "Nombre erroneo.";
-                }
-            }else{
-                nameError.text = "Nombre requerido.";
-            }
-            
-            if (firstSurnameInput.text != "")//surname
-            {
-                if (!rgx.IsMatch(firstSurnameInput.text))
-                {
-                    firstSurnameError.text = "Apellido erroneo.";
-                }
-            }else{
-                firstSurnameError.text="Apellido requerido.";
-            }
-
-            if (secondSurnameInput.text != "")//second surname
-            {
-                if (!rgx.IsMatch(secondSurnameInput.text))
-                {
-                    secondSurnameError.text = "apellido erroneo.";
-                }
-            }
-
-            System.DateTime d;//date of birth
-            if (dayInput.text.Length == 1)
-                dayInput.text = "0"+dayInput.text;
-            if (monthInput.text.Length == 1)
-                monthInput.text = "0"+monthInput.text;
-            bool chValidity = System.DateTime.TryParseExact(
-            dayInput.text+"/"+monthInput.text+"/"+yearInput.text,
-            "dd/MM/yyyy",
-            CultureInfo.InvariantCulture,
-            DateTimeStyles.None,
-            out d);
-            if (!chValidity)
-            {
-                dateOfBirthError.text="Fecha no válida.";
-            }
-            if (d.Year >= System.DateTime.Now.Year || d.Year < 1900)
-            {
-                dateOfBirthError.text="Año no válido.";
-            }
-
-        }
-        catch (System.Exception ex)
-        {
-            registerMessage.text = ex.Message;
-        }
-        /*
         string email = emailInput.text, password = passwordInput.text, repeatPassword = repeatPasswordInput.text,
                name = nameInput.text, firstSurname = firstSurnameInput.text, secondSurname = secondSurnameInput.text,
                day = dayInput.text, month = monthInput.text, year = yearInput.text,
@@ -162,7 +60,7 @@ public class RegisterScript : MonoBehaviour
         else
         {
             StartCoroutine(Register(email, password, name, firstSurname, secondSurname, day, month, year, country));
-        }*/
+        }
     }
 
     void CountryDropdownOnValueChanged()
